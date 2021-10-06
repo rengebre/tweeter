9240fcfd62dea34938a82d1e0dffb50821f24476
Russell Engebretson <russell.engebretson@gmail.com>
2021-10-06 04:48:45 +0000

    Add tweet containers for the main page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,21 +4,29 @@ const maxTweetChars = 140;
 $(document).ready(function() {
 
   // Event to keep track of the character count of new tweets
-  $(".tweet-text").on("input", function(e) {
+  $('.tweet-text').on("input", function(e) {
     const inputLength = $(this).val().length;
-    let $counterHandle;
+    
+    // The following code is to navigate through the DOM to find the correct element instead of using the $("#tweet-char-counter") handle directly. This is what compassed has informed us to do, but I really don't like it - it's very unmaintanable and unreadable.
+    
+    // let $counterHandle;
 
-    // navigate through the DOM to find the correct element instead of using the $(".tweet-char-counter") handle directly
-    for (const element of $(this).siblings()) {
-      for (const child of $(element).children()) {
-        if($(child).hasClass("tweet-char-counter")) {
-          $counterHandle = $(child);
-        }
-      }
-    }
+    // for (const element of $(this).siblings()) {
+    //   for (const child of $(element).children()) {
+    //     if($(child).hasClass("tweet-char-counter")) {
+    //       $counterHandle = $(child);
+    //     }
+    //   }
+    // }
   
+    
+    // implement the counter using the element ID of the counter
+    
+    const $counterHandle = $('#tweet-char-counter');
+    
     $counterHandle.val(maxTweetChars - inputLength);
 
+    // Change the color style of the counter element if it goes negative. Change back if it becomes positive again
     if ($counterHandle.val() < 0) {
       $counterHandle.css('color', 'red');
     } else {
